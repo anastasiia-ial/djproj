0e1775531c83093ffeb6f929522c3f067bc35c1c
anastasiia-ial <80840113+anastasiia-ial@users.noreply.github.com>
2022-12-17 21:47:35 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -9,16 +9,24 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
-from django.contrib.staticfiles import finders
-# from django.http import FileResponse
-# import io
-# from reportlab.pdfgen import canvas
-# from reportlab.lib.units import cm
-# from reportlab.lib.pagesizes import A4
 
+def render_pdf_view(request):
+    template_path = 'user_printer.html'
+    context = {'myvar': 'this is your template context'}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
 
- 
-
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funny view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 
 def delete_sku(request, sku_id):
     sku = Sku.objects.get(id=sku_id)
